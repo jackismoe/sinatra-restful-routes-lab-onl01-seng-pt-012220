c5b7b33d052ac6e36afe5345b0ba0d1cf686cef1
ohjacksn <jacksonmo@me.com>
2020-04-26 12:29:13 -0400

    lab update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,5 +41,16 @@ class ApplicationController < Sinatra::Base
     redirect to '/recipes/#{@recipe.id}'
   end
 
+  # creates a recipe
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to '/recipes/#{@recipe.id}'
+  end
 
+  # destroy action
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 end
